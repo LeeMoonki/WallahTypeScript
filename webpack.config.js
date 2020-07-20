88cb357bef060fff313ae6b13248c45c8c608e47
Moonki Lee <shinbaek89@gmail.com>
2020-07-20 23:38:25 +0900

    init the project

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/index.ts'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    // attempt to resolve these extensions in order
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+};
