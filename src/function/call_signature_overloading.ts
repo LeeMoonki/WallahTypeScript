ea8d4255d698868b88469645de4a015268c14b19
Moonki Lee <shinbaek89@gmail.com>
2020-08-31 10:52:24 +0900

    함수 오버로딩

---
+++ src/function/call_signature_overloading.ts
@@ -0,0 +1,39 @@
+type Reservation = {
+  // ...
+}
+
+// Reserve라는 예약 API를 설계
+type Reserve = {
+  (from: Date, to: Date, destination: string): Reservation
+  (from: Date, destination: string): Reservation // 편도
+}
+
+// 구현
+// 편도 타입이 없다면 다음 코드는 자동 추론으로 인해 문제가 없는 코드가 된다.
+// 하지만 위에서 처럼 함수 타입을 정의하면 Reserve 내의 타입들은 유니온 연산이 되지만
+// 구현부에선 특정 타입을 나타내야 하므로 오류가 발생한다.
+
+/*
+let reserve: Reserve = (from, to, destination) => {
+  return {
+    // ...
+  };
+}
+*/
+
+// 따라서 다음과 같이 구현해야 한다.
+let reserve: Reserve = (
+  from: Date,
+  toOrDestination: Date | string,
+  destination?: string
+) => {
+  if (toOrDestination instanceof Date && destination !== undefined) {
+    // 왕복
+  } else if (typeof toOrDestination === 'string') {
+    // 편도
+  }
+
+  return {
+    // ...
+  };
+}
--- src/function/signature.ts
+++ src/function/signature.ts
@@ -3,4 +3,13 @@ function sum(a: number, b: number): number {
 }
 
 // 함수 sum의 호출 시크니처(call signature) 또는 타입 시크니처(type signature)
+// 또는 단축형 호출 시그니처(shorthand call signature)로도 불린다.
 type Sum = (a: number, b: number) => number
+
+// 단축형이 아니라 전체 호출 시그니처는 다음과 같다.
+
+/*
+type Sum = {
+  (a: number, b: number): number
+}
+*/
