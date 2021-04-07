9e83cf4edc83515a641efdcd5333906a2c7251aa
Junep <prepared@inflearn.com>
2021-04-07 11:02:25 +0900

    up: typeof와 keyof typeof 결과를 enum_using_const_object에 추가

--- src/docs/enum_using_const_object.ts
+++ src/docs/enum_using_const_object.ts
@@ -4,4 +4,7 @@ export const ENUM_OBJECT_CONFIG = {
   BAZ: 'baz',
 } as const;
 
+export type TypeofConstObject = typeof ENUM_OBJECT_CONFIG;
+export type KeyofTypeofConstObject = keyof typeof ENUM_OBJECT_CONFIG;
+
 export type EnumObjectConfig = typeof ENUM_OBJECT_CONFIG[keyof typeof ENUM_OBJECT_CONFIG];
